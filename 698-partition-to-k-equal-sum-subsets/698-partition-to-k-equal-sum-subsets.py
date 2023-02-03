82b3f9923778373a245eba0dc811c57fa02ceed1
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-02-03 22:39:53 +0300

    Time: 121 ms (72.28%), Space: 14 MB (74.25%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
@@ -1,26 +1,18 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        target, m = divmod(sum(nums), k)
+        if m: return False
+        dp, n = [0]*k, len(nums)
         nums.sort(reverse=True)
-        
-        total = sum(nums)
-        target = total // k
-        #print(nums, target)
-        used = [False] * len(nums)
-        if total % k or nums[0] > target or (nums[0] < target and nums[0] + nums[-1] > target):
-            #print('here')
-            return False
-        def search(i,k,subsetsum):
-            if k==0:
-                return True
-            if subsetsum == target:
-                return search(0,k-1,0) # search number for the other remaining subsets
-            for place in range(i,len(nums)):
-                if used[place] or subsetsum + nums[place] > target:
-                    continue
-                used[place]=True
-                if search(place+1,k,subsetsum + nums[place]):
+        def dfs(i):
+            if i == n:
+                return len(set(dp)) == 1
+            for j in range(k):
+                dp[j] += nums[i]
+                if dp[j] <= target and dfs(i+1):
                     return True
-                used[place]=False
+                dp[j] -= nums[i]
+                if not dp[j]: break
             return False
-        return nums[0] <= target and search(0,k,0)
+        return nums[0] <= target and dfs(0)
             
