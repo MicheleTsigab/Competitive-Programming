687578b525261136392c9c5feb6ef96898c9fbb9
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-02-13 18:34:42 +0300

    Time: 106 ms (73.14%), Space: 14.5 MB (85.39%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -12,9 +12,11 @@ class Solution:
         while left < right:
             if s[left] != s[right]:
                 #check if the string a palindrom if we removed at left
-                s1 = [s[i] for i in range(len(s)) if i!=right]
+                #s1 = [s[i] for i in range(len(s)) if i!=right]
+                s1 = s[left+1:right+1]
                 #check if the string a palindrom if we removed at right
-                s2 = [s[i] for i in range(len(s)) if i!=left]
+                #s2 = [s[i] for i in range(len(s)) if i!=left]
+                s2 = s[left:right]
                 if s1[::-1]==s1 or s2[::-1]==s2:
                     return True
                 return False
