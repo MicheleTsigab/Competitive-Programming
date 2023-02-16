6ec9a4457fb2d9e62711ffc2373d5de7f44295a5
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-02-16 18:17:45 +0300

    Time: 619 ms (27.70%), Space: 16.4 MB (93.85%) - LeetHub

--- 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.py
+++ 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.py
@@ -1,21 +1,36 @@
 class Solution:
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
         
-        res = ''
-        
-        for word in sorted(dictionary):
-            x= self.longest(word,s)
-            res = max(res,x , key= len)
-        return res
-    def longest(self,target,s):
-        ans = []
-        pointer_t = 0
-        pointer_s = 0
+        cur = ''        
+        for word in dictionary: 
+            found= self.longest(word,s)
+            if not found:
+                continue
+    
+            #compare to find longest
+            if len(word) > len(cur):
+                cur = word
+
+            #if tie(same length) choose lexicographically smaller one
+            elif len(word)==len(cur):
+                cur = min(word,cur)
+        return cur
+    
+    def longest(self,target,s): #O(len(target))
+        """
+        return True if target is a subsequence of s
+        """
+        count,pointer_t,pointer_s = 0,0,0
         while pointer_t < len(target) and pointer_s < len(s):
             if s[pointer_s]==target[pointer_t]:
-                ans.append(s[pointer_s])
+                count +=1
                 pointer_t +=1
                 pointer_s +=1
             else:
                 pointer_s +=1
-        return ''.join(ans) if len(ans)==len(target) else ''
+        return count==len(target)
+    #N = len(dictionary)
+    #X = average length of the words in dictionary
+    #O(NXlogN + XN)
+    #improved
+    #O(NX)
