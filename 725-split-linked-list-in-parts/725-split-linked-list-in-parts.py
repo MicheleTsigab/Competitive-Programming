623e6a22e40b52a664ac97c735eb7e8fb2b1ea9c
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-02-28 21:32:38 +0300

    Time: 36 ms (86.99%), Space: 14.2 MB (96.26%) - LeetHub

--- 725-split-linked-list-in-parts/725-split-linked-list-in-parts.py
+++ 725-split-linked-list-in-parts/725-split-linked-list-in-parts.py
@@ -6,27 +6,22 @@
 class Solution:
     def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
         result = [None for _ in range(k)]
-        dummy = ListNode(0,next=head)
-        current = dummy
+        current = head
         slow = head
-        fast = dummy
         length = 0
-        while current.next:
+        while current:
             current = current.next
             length += 1
         rem = length % k
         part = length // k
-        part_1 = part + 1
         for i in range(len(result)):
             lst = slow
             result[i]=lst
-            p = part_1 if i < rem else part
+            p = part + 1 if i < rem else part
             for j in range(p-1):
                 if lst:
                     lst= lst.next
             if lst:
-                prev = lst
-                slow = lst.next
-                prev.next = None
+                lst.next,slow = None,lst.next
             
         return result
