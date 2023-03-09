3d2043f359e05ec529a064b2233cf75c5439434f
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-03-09 18:03:08 +0300

    Time: 861 ms (68.81%), Space: 19.4 MB (38.99%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.py
+++ 525-contiguous-array/525-contiguous-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         longest = 0
-        prefix = {0:-1}
+        prefix = {0:0}
         total = 0
         for right, n in enumerate(nums):
             n = 1 if n else -1
@@ -9,7 +9,7 @@ class Solution:
            # print(prefix,total,right)
             if total in prefix:
                 left = prefix.get(total)
-                longest = max(right - left ,longest)
+                longest = max(right - left + 1 ,longest)
             else:
-                prefix[total]=right
+                prefix[total]=right + 1
         return longest
