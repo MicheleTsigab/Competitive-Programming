b41f9df50f4fb533e1a18b2430f84e541283a395
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-03-10 21:09:33 +0300

    Time: 1336 ms (59.34%), Space: 29 MB (10.21%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -4,11 +4,13 @@ class Solution:
         total = 0
         result = math.inf
         inc_queue = deque() #monotonic increasing 
+        inc_queue.append([0,-1])
         for right,n in enumerate(nums):
             total +=n
             #total starting from 0 to right
-            if total >=k:
-                result=min(result,right+1)
+   
+            #if total >=k:
+             #   result=min(result,right+1)
             
             #maintain a monotonically increasing queue
             while inc_queue and total < inc_queue[-1][0] :
@@ -17,12 +19,11 @@ class Solution:
             inc_queue.append((total,right))
             #if current window is greater than k we try to minimize the window by popping from left
             # we pop and maintain
-            cur=-1
+           # cur=-1
             while inc_queue and (inc_queue[-1][0] - inc_queue[0][0]) >=k:
-                cur = inc_queue[0][1]
-                inc_queue.popleft()
-            if cur!=-1:
-                result=min(result,right-cur)
+                left = inc_queue.popleft()[1]
+                
+                result=min(result,right-left)
             
 
             
