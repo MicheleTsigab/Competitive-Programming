3b6f8d391e7bb8aabca4014ac0686a05110974c0
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-03-13 22:12:32 +0300

    Time: 29 ms (90.81%), Space: 13.9 MB (30.37%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -1,46 +1,21 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        
         st = []
-        for p in path:
-            if not st or p!='/':
-                st.append(p)
-                continue                    
-            if st and p=='/':
-                if st and st[-1]=='/':
-                    continue
-                if len(st) > 2 and st[-1]=='.' and st[-2]=='.' and st[-3]=='/':
-                    
-                    st.pop() #.
-                    st.pop() #.
-                    if len(st)<=1: #already in base case
-                        continue
-                    else:
-                        st.pop() #trailing /
-                        
-                    while st and st[-1]!='/': #moving up
-                        st.pop()
-                    continue
-                        
-                elif len(st) > 1 and st[-1]=='.' and st[-2]=='/':
-                    st.pop() #.   
-                else:
-                    st.append(p)
-
-        if len(st) > 2 and st[-1]=='.' and st[-2]=='.' and st[-3]=='/':
-            st.pop()
-            if st[-1]=='.':
-                st.pop()
-                if len(st)>1: #already in base case
-                    st.pop() #trailing /
-                    
-                while st and st[-1]!='/': #moving up
+        m = path.split('/')
+        #print(m)
+        for i in range(len(m)):
+            p = m[i]
+            if p=='' or p=='.':
+                continue
+            if p=='..':
+                if st:
                     st.pop()
-        elif len(st) > 1 and st[-1]=='.' and st[-2]=='/':
-            st.pop()
-            
-        
-        if len(st) > 1 and st[-1]=='/':
-            st.pop()
-            
-        return ''.join(st)
+                continue
+            st.append(p)
+        result = []
+        result.append('/')
+        for i,n in enumerate(st):
+            result.append(n)
+            if i!=len(st)-1:
+                result.append('/')
+        return ''.join(result)
