c9677f9f78024243c5d6d38ccd24568cfc60b7f0
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-03-17 22:04:57 +0300

    Time: 90 ms (52.60%), Space: 15.4 MB (38.12%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,9 +1,9 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         """
-         0 1 2 3 4 5
-        [5,7,7,8,8,10] t = 8
-               l  r
+         0 1 2 3 4  5 6
+        [5,7,7,8,8,10] t =11
+               l
                m
         flag = True
         l =0 , 3
@@ -12,31 +12,8 @@ class Solution:
          O(log(n))
          2 binary searches over the original 
          """
-        l = 0
-        r = len(nums) - 1
-        flag = False #indicate not found
-        #left most
-        while l<=r:
-            mid = l + (r- l )//2
-            if target > nums[mid]:
-                l = mid + 1
-            else:
-                if nums[mid]==target:
-                    flag = True
-                r = mid - 1
-        if not flag:
+        l = bisect_left(nums,target)
+        r = bisect_right(nums,target) - 1
+        if l>=len(nums) or nums[l]!=target:
             return [-1,-1]
-        ans = []
-        
-        ans.append(l)
-        #right most
-        l = 0
-        r = len(nums) - 1
-        while l<=r:
-            mid = l + (r- l )//2
-            if target >= nums[mid]:
-                l = mid + 1
-            else:
-                r = mid - 1
-        ans.append(r)
-        return ans
+        return [l,r]
