2939c210a0dae48750df1755b9eb66cf4ebee9b4
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-03-17 22:45:25 +0300

    Time: 923 ms (14.28%), Space: 20.2 MB (5.65%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,40 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        """
+        [1,2,3,4,5,6,7,8,9,10], days = 5
+         0 1 2 3 4 5 6 7 8 9
+         0  11 55
+         l m
+        """
+        #binary search
+        l = 0
+        r = sum(weights)
+        ans = math.inf
+        while l<=r:
+            mid = l + (r - l)//2
+            if self.possible(mid,weights,days):
+                r = mid - 1
+               # print(ans)
+                ans = min(ans,mid)
+            else:
+                l = mid + 1
+        return ans
+                
+    def possible(self,cap,weights,days):
+        count = 0
+        total = 0
+        
+        for i in range(len(weights)): 
+            
+            if weights[i] > cap:
+                return False
+            if weights[i] + total > cap:
+                count +=1
+                total = 0
+            
+            total += weights[i]
+        if total:
+            count += 1
+        if count > days:
+            return False
+        return True
