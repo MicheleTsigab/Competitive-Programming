f669afda4484b938ab16e2d83829e92d6b99b066
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-03-30 22:20:12 +0300

    Time: 134 ms (92.72%), Space: 20.5 MB (73.15%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        l = 0
+        r = 1000
+        ans = -1000
+        while l <= r:
+            mid = l + (r-l)//2
+            x =len(citations) - bisect_left(citations,mid)
+            if x >= mid:
+                ans = max(ans,mid)
+                l = mid + 1
+            else:
+                r = mid - 1
+        return ans
