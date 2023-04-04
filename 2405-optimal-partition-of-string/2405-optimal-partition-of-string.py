932dc326e68b09f66a5522a237eaf4eb52155e81
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-04-04 21:35:31 +0300

    Time: 111 ms (77.57%), Space: 14.5 MB (90.90%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        
+        seen = set()
+        count = 1
+        for i in s:
+            if i in seen:
+                seen.clear()
+                count+=1
+            
+            seen.add(i)
+        return count
