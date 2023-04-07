2d5126c21ec22bb7fe8a9181684f78f35bfb7345
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-04-07 22:47:05 +0300

    Time: 141 ms (30.43%), Space: 16.1 MB (6.80%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -10,18 +10,19 @@ class Solution:
             if grid[r][c] == 1 or (r,c) in seen:
                 return True
             seen.add((r,c))
-            dirs = [(-1,0),(1,0),(0,1),(0,-1)]
-            found = True
-            for dy, dx in dirs:
-                if not search(r + dy, c + dx):
-                    found = False
-            return found
+            down = search(r+1,c)
+            up = search(r-1,c)
+            left = search(r,c-1)
+            right = search(r,c+1)
+            
+            return up and down and left and right
+
         
         count = 0
-        for i in range(rows):
-            for j in range(cols):
-                if not grid[i][j] and (i,j) not in seen:
-                    if search(i,j):
+        for r in range(rows):
+            for c in range(cols):
+                if not grid[r][c] and (r,c) not in seen:
+                    if search(r,c):
                         count += 1
         return count
                     
