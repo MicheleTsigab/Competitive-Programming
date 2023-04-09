472256fa83fa23fd6f620d5686311cf8f816a011
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-04-09 19:30:30 +0300

    Time: 367 ms (26.85%), Space: 21.7 MB (21.00%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -14,15 +14,14 @@ class Solution:
             arr.append(node.val)
             inorder(node.right)
         inorder(root)
-        return self.sortedArrayToBST(arr)
+        return self.sortedArrayToBST(arr,l = 0, r = len(arr) -1)
         
-    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        if len(nums) == 0:
+    def sortedArrayToBST(self, nums, l,r):
+        
+        if l > r:
             return None
-        l = 0
-        r = len(nums) - 1
         mid = ceil((l + r)/2)
         root = TreeNode(val = nums[mid])
-        root.left = self.sortedArrayToBST(nums[:mid])
-        root.right = self.sortedArrayToBST(nums[mid+1:])
+        root.left = self.sortedArrayToBST(nums,l,mid - 1)
+        root.right = self.sortedArrayToBST(nums,mid + 1,r)
         return root
