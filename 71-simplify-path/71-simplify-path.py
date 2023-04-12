bd252907f717e1a31d36d4dc3cb697408e85a3f5
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-04-12 14:37:17 +0300

    Time: 31 ms (82.06%), Space: 14.1 MB (5.75%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -13,15 +13,7 @@ class Solution:
                     st.pop()
                 continue
             st.append(p)
-            if i!=len(m)-1:
-           #     print('')
-                st.append('/')
-        result = []
-        result.append('/')
-        # for i,n in enumerate(st):
-        #     result.append(n)
-        #     if i!=len(st)-1:
-        #         result.append('/')
+            st.append('/')
         if len(st) > 1 and st[-1]=='/':
             st.pop()
         return ''.join(st)
