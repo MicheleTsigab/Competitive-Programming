19126bc98114c241ae551e3181bebdc3514f30ae
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-04-12 18:16:20 +0300

    Time: 39 ms (23.06%), Space: 14 MB (13.55%) - LeetHub

--- 306-additive-number/306-additive-number.py
+++ 306-additive-number/306-additive-number.py
@@ -1,42 +1,28 @@
 class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
-        """
-        112358
         
-        
-        length
-        1
-        i = 0
-        1 11 112 1123 11235 112358
-        i = 1
-    1   12 123 1235 12358
-    i = 2
-2 23 235 2358
-
-        """
-        found = False
         def search(i,path):
-            nonlocal found
             if len(path) >= 3:
                 if path[-1] != (path[-2] + path[-3]):
                     return False
-            if i == len(num) and len(path) >=3:
+            if i == len(num) and len(path) >= 3:
                 return True
             for j in range(i,len(num)):
                 new = num[i:j+1]
-                
-                if len(new) > 1 and new[0]=='0':
+
+                if len(new) > 1 and new[0] == '0':
                     continue
+                    
                 path.append(int(new))
                 if search(j+1,path):
                     return True
                 path.pop()
             return False
-                
+
         for i in range(len(num)):
             c = num[:i+1]
-            if len(c) > 1 and num[0]=='0':
+            if len(c) > 1 and num[0]=='0': #leading zero
                 return False
             if search(i+1,[int(c)]):
                 return True
-        return found
+        return False
