93ae797622b2717c28bb154d92c16239db4db03d
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-04-17 19:29:02 +0300

    Time: 40 ms (61.54%), Space: 13.9 MB (53.85%) - LeetHub

---
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def addMinimum(self, word: str) -> int:
+        word_ptr = 0
+        target = 'abc' * len(word)
+        tar_ptr = 0
+        count = 0
+        
+        while word_ptr < len(word):
+                            
+            if word[word_ptr]!= target[tar_ptr]:
+                tar_ptr += 1
+                count += 1
+
+            if word[word_ptr] == target[tar_ptr]:
+                word_ptr += 1
+                tar_ptr += 1
+                
+        if word[-1] == 'a':
+            count += 2
+        if word[-1] == 'b':
+            count += 1
+        return count
