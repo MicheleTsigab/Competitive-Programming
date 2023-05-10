0230b55653047270f0581f90c4adc18a88f58e15
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-05-10 21:08:06 +0300

    Time: 6507 ms (19.06%), Space: 70 MB (34.38%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -1,13 +1,14 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
         x = [-1 for i in range(n)]
-        
-        for i in range(2,n):
+        i = 2
+        while i * i < n:
             if x[i] == -1:
                 j = i + i
                 while j <n:
                     x[j] = 1
                     j+=i
+            i+=1
      #   print(x)
         cnt = 0
         for i in range(2,len(x)):
