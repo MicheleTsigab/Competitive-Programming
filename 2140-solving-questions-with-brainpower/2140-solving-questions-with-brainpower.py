f91654bf5267b83e861c32e853d490db50915f01
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-05-12 19:59:17 +0300

    Time: 1716 ms (45.68%), Space: 148.5 MB (41.98%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,9 +1,12 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         
-        @lru_cache(None)
+        dp = [-1] * len(questions)
         def search(i):
             if i >= len(questions):
                 return 0
-            return max(questions[i][0] + search(i+questions[i][1] + 1), search(i+1))
+            if dp[i]!=-1:
+                return dp[i]
+            dp[i] = max(questions[i][0] + search(i+questions[i][1] + 1), search(i+1))
+            return dp[i]
         return search(0)
