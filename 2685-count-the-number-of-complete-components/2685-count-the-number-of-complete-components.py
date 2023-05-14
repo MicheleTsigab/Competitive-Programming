a42ef6e8530acbfbe8d9fbd4f3118cc985603988
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-05-14 23:56:48 +0300

    Time: 775 ms (7.69%), Space: 16.9 MB (7.69%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -3,47 +3,41 @@ class Dsu:
         self.comp_count = n
         self.parent = [i for i in range(n)]
         self.components = [1 for i in range(n)]
+        self.edges = [0 for i in range(n)]
     def find(self,node):
         if self.parent[node] == node:
         	return node
         self.parent[node] = self.find(self.parent[node])
         return self.parent[node]
     def union(self,n1,n2):
+
         root1 = self.find(n1)
         root2 = self.find(n2)
+        self.edges[root1]+=1
         if root1 == root2:
             return 0
-        if self.components[root1] > self.components[root2]:
-            self.components[root1]+=self.components[root2]
-            self.components[root2] = 0
-            self.parent[root2] = root1
-        else:
-            self.components[root2]+=self.components[root1]
-            self.components[root1] = 0
-            self.parent[root1] = root2
+
+        self.components[root1]+=self.components[root2]
+        self.edges[root1]+=self.edges[root2]
+        self.parent[root2] = root1
         self.comp_count -=1
         return 1
-    def num_comp(self):
-        return self.comp_count
+    def num_complete(self):
+        ans = 0
+        n = len(self.components)
+        for i in range(n):
+            if self.find(i) == i:
+                x = self.components[i]
+                if self.edges[i] ==  (x * (x-1)//2):
+                    ans += 1
+        return ans
+
             
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
         graph = Dsu(n)
-        edge_set = set()
         for x,y in edges:
             graph.union(x,y)
-            edge_set.add((x,y))
-            edge_set.add((y,x))
-        discard = set()
-        cnt = graph.num_comp()
-        for i in range(n):
-            for j in range(i+1,n):
-                root1 = graph.find(i)
-                root2 = graph.find(j)
-                if root1 == root2 and (i,j) not in edge_set:
-                    if root1 in discard:
-                        continue
-                    cnt -=1
-                    discard.add(root1)
-        return cnt
+
+        return graph.num_complete()
                     
