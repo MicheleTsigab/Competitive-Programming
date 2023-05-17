2b7d85f2adc2a38a3c1b9eede1840e8b59a720c7
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-05-18 00:05:22 +0300

    Time: 90 ms (92.61%), Space: 17.6 MB (26.14%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -1,23 +1,21 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
         
-        mx = 0
+        @lru_cache(None)
+        def search(i,o):
+            if i >= len(nums):
+                return o
+            return max(search(i+1,o | nums[i]),search(i+1,o))
+        mx = search(0,0)
+        ans = 0
+        @lru_cache(None)
         def search(i,o):
             nonlocal mx
             if i >= len(nums):
-                mx = max(mx,o)
-                return
-            search(i+1,o | nums[i])
-            search(i+1,o)
-        search(0,0)
-        ans = 0
-        def search(i,o):
-            nonlocal ans
-            if i >= len(nums):
-                if o == mx:
-                    ans+=1
-                return
-            search(i+1,o | nums[i])
-            search(i+1,o)
-        search(0,0)
-        return ans
+                if mx == o:
+                    return 1
+                else:
+                    return 0
+            return search(i+1,o | nums[i]) + search(i+1,o)
+        
+        return search(0,0)
