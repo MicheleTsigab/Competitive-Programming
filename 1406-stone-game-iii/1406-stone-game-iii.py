a2ebd8819d1c62dda56c8d094fe6c10a8ac05c65
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-05-27 22:48:29 +0300

    Time: 3426 ms (40.09%), Space: 222.2 MB (22.42%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.py
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -2,15 +2,15 @@ class Solution:
     def stoneGameIII(self, stoneValue: List[int]) -> str:
         p = sum(stoneValue)
         @cache
-        def solve(i,alice):
+        def solve(i):
             if i >= len(stoneValue):
                 return 0
             N = len(stoneValue)
-            x = sum(stoneValue[i:min(i+1,N)]) - solve(i+1,not alice)
-            y = sum(stoneValue[i:min(i+2,N)]) - solve(i+2,not alice)
-            z = sum(stoneValue[i:min(i+3,N)]) - solve(i+3,not alice)
+            x = sum(stoneValue[i:min(i+1,N)]) - solve(i+1)
+            y = sum(stoneValue[i:min(i+2,N)]) - solve(i+2)
+            z = sum(stoneValue[i:min(i+3,N)]) - solve(i+3)
             return max(x,y,z)
-        ans = solve(0,True) 
+        ans = solve(0) 
        # print(ans)
         if ans==0:
             return 'Tie'
