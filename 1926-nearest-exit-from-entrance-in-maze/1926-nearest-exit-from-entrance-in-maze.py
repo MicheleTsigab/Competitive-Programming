94976dd9f25bd375019dc80705da3c864cb3ecb9
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-06-01 17:22:53 +0300

    Time: 880 ms (47.86%), Space: 18.1 MB (15.41%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -3,37 +3,21 @@ class Solution:
         
         rows = len(maze)
         cols = len(maze[0])
-        exit = set()
-        for i in range(cols):
-            if [rows-1,i]!=entrance and maze[rows-1][i]=='.':
-                exit.add((rows-1,i))
-            if [0,i]!=entrance and maze[0][i]=='.':
-                exit.add((0,i))
-        for i in range(rows):
-            if [i,cols-1]!=entrance and maze[i][cols-1]=='.':
-                exit.add((i,cols-1))
-            if [i,0]!=entrance and maze[i][0]=='.':
-                exit.add((i,0))
-                
+        dirs = [[1,0],[0,1],[-1,0],[0,-1]]
         q = deque()
-        for i in exit:
-            q.append(i)
-        l = 0
         seen = set()
+        q.append([entrance[0],entrance[1],0])
+        seen.add((entrance[0],entrance[1]))
+
         while q:
-            
-            for _ in range(len(q)):
-                r,c =q.popleft()
-                if (r,c) not in seen:
-                    seen.add((r,c))
-                if [r,c] == entrance:
-                    return l
-                dirs = [[1,0],[0,1],[-1,0],[0,-1]]
-                for dr,dc in dirs:
-                    rn = dr + r
-                    cn = dc + c
-                    if 0<=rn<rows and 0<=cn<cols and (rn,cn) not in seen and maze[rn][cn]=='.':
-                        q.append([rn,cn])
-                        seen.add((rn,cn))
-            l+=1
+            r,c,dist =q.popleft()
+            if [r,c]!=entrance and (r==0 or r == rows - 1 or c == 0 or c == cols-1):
+                return dist
+
+            for dr,dc in dirs:
+                rn = dr + r
+                cn = dc + c
+                if 0<=rn<rows and 0<=cn<cols and (rn,cn) not in seen and maze[rn][cn]=='.':
+                    q.append([rn,cn,dist+1])
+                    seen.add((rn,cn))
         return -1
