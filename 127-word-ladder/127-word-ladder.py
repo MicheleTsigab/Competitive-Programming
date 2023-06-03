f098fcdbc8713a26917656294607782383f640ec
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-06-03 11:22:20 +0300

    Time: 616 ms (20.25%), Space: 17.6 MB (57.18%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,6 +1,6 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        
+        wordList = set(i for i in wordList)
         q = deque()
         q.append([beginWord,1])
         seen = set()
@@ -9,16 +9,12 @@ class Solution:
             node,leng = q.popleft()
             if node == endWord:
                 return leng
-            for j in range(len(wordList)):
-                nei = wordList[j]
-                if nei not in seen:
-                    count = 0
-                    for x,y in zip(node,wordList[j]):
-                        if x!=y:
-                            count +=1
-                        if count >=2:
-                            break
-                    if count == 1:
+            for i in range(len(node)):
+                for c in string.ascii_lowercase:
+                    nei = list(node)
+                    nei[i] = c
+                    nei = ''.join(nei)
+                    if nei not in seen and nei in wordList:
                         q.append([nei,leng+1])
                         seen.add(nei)
         return 0
