06bc20688d8124c68c7ed8d350d222ddba04a55f
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-07-10 22:52:39 +0300

    Time: 1653 ms (83.24%), Space: 61.2 MB (66.11%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -29,19 +29,12 @@ class Solution:
         dsu = UnionFind(n+1)
         for a,b,d in roads:
             dsu.union(a,b)
-            p1 = dsu.find(a)
-            p2 = dsu.find(b)
-            cost[p1]= min(cost[p1],d)
-            cost[p2]=min(cost[p2],d)
+            cost[a]= min(cost[a],d)
+            cost[b]=min(cost[b],d)
         p1 = dsu.find(1)
-        p2 = dsu.find(n)
-        #print(cost)
         ans = math.inf
         for i in range(1,n+1):
             x= dsu.find(i)
-    #        if i == 12:
-     #           print(x,p1,p2,cost[12])
-
-            if x==p1 or x==p2:
+            if x==p1:
                 ans = min(cost[i],ans)
         return ans
