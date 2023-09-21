1d746519896255847dbc9cee5e636175ff3269c9
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-09-21 22:35:40 +0300

    Time: 1409 ms (71.23%), Space: 33.1 MB (93.15%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
@@ -4,7 +4,7 @@ class Solution:
         4,3, 3,1 1,2
         0 1 2 3 4 5 6 7 8 9 10
         p p p p g g g b
-                p g g b 
+                p g g b
                   p p p g b
         """
         pair = sorted(zip(growTime,plantTime),reverse=True)
