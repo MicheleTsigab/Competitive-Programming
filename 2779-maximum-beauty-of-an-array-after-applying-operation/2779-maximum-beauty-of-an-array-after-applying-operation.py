bd24ab486e973b3c1e070b05756307358e3bdfbc
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-11-15 21:22:32 +0300

    Time: 4116 ms (5.40%), Space: 30.5 MB (55.86%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,27 +1,20 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         nums.sort()
-        ranges = []
-        for i in nums:
-            ranges.append([i-k,i+k])
         ans = 1
-        #print(ranges)
-        for i in range(len(ranges)):
-            _,end1 = ranges[i]
+        for i in range(len(nums)):
+            end1 = nums[i]+k
             left = i + 1
-            right = len(ranges)-1
-            found = -1
+            right = len(nums)-1
+            found = i
             while left <= right:
                 mid = (left + right)//2
 
-                if end1 in range(ranges[mid][0],ranges[mid][1]+1):
-                    #answer found
-                    found = max(found,mid)
-                #    print(found,'hello')
+                if end1 in range(nums[mid]-k,nums[mid]+k+1):
+
+                    found = mid
                     left = mid + 1
                 else:
                     right = mid - 1
-            #print(found,i)
-            if found!=-1:
-                ans = max(found-i+1, ans)
+            ans = max(found-i+1, ans)
         return ans
