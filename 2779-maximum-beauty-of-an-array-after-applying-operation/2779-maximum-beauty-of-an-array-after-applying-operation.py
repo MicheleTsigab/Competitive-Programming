905fbe685b774c5f09a291054d470a7bb4a49f30
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-11-15 21:23:45 +0300

    Time: 3962 ms (5.40%), Space: 30.7 MB (22.07%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -6,15 +6,12 @@ class Solution:
             end1 = nums[i]+k
             left = i + 1
             right = len(nums)-1
-            found = i
             while left <= right:
                 mid = (left + right)//2
 
                 if end1 in range(nums[mid]-k,nums[mid]+k+1):
-
-                    found = mid
                     left = mid + 1
                 else:
                     right = mid - 1
-            ans = max(found-i+1, ans)
+            ans = max(left-i, ans)
         return ans
