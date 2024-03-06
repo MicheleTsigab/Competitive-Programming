70635189bd3c2167123038de9864edb39cce0e42
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2024-03-06 11:05:49 +0300

    Time: 64 ms (75.50%), Space: 17.3 MB (73.50%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -4,13 +4,9 @@ class Solution:
         r = len(s)-1
         delete = 0
         while l < r and s[l]==s[r]:
-            right = s[r]
-            left = s[l]
-            while s[l]==right and l<r:
-                delete+=1
+            comp = s[r]
+            while s[l]==comp and l<r:
                 l+=1
-            while s[r]==left and r>=l:
-                delete+=1
+            while s[r]==comp and r>=l:
                 r-=1
-   #     print(s[l],s[r],l,r)
-        return len(s)-delete
+        return r-l+1
